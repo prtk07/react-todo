97b8c11eab0eebd15147bb1b2ff730457834767a
Prateek Verma <prateekverma70@gmail.com>
2021-05-27 19:38:55 +0530

    checkbox added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
     return (
         <div className='header'>
             <h2>DOtodo</h2>
-            <p onClick={props.togglePopup}>Sign Out</p>      
+            <p onClick={props.togglePopup}>Sign Out</p>     
         </div>
     )
 }
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,22 +1,40 @@
 import React from "react";
-import './Task.css';
+import { Checkbox, FormControlLabel } from "@material-ui/core";
+import "./Task.css";
 
 function Tasks(props) {
-    function DeleteItem(e){
-        // e.preventDefault();
-        let ind = e.target.parentNode.id;
-        console.log(ind);
-        props.removeItem(ind);
-    }
+  function DeleteItem(e) {
+    let ind = e.target.parentNode.id;
+    console.log(ind);
+    props.removeItem(ind);
+  }
+
+  function handleCheckbox(e) {
+    console.log(e.target.name);
+  }
+
   return (
     <div>
       {props.list.map((val, index) => {
         return (
-            <div className="item-class" key={index} id={index}>
-                <p>{val}</p>
-                <p className="close-button" onClick={DeleteItem}>close</p>
+          <div className="item-class" key={index} id={index} completed="false">
+            <div className="content">
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={handleCheckbox}
+                    color="primary"
+                    name={`${index}`}
+                  />
+                }
+                label={val}
+              />
             </div>
-        )
+            <p className="close-button" onClick={DeleteItem}>
+              close
+            </p>
+          </div>
+        );
       })}
     </div>
   );
